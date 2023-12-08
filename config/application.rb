02670ec2b80da8b6ec928f47d4378b3d699bce16
Jackson Coleman <wcoleman@colgate.edu>
2023-12-07 21:36:44 -0600

    Heroku bugtesting

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,7 @@ module CcBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*' # Update with your React frontend origin
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: false
       end
     end
   end
